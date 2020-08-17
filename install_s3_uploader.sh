1b134ccb6909c13a734fe73beee8a7e38df7da84
Jeff Rade <jeffrade@gmail.com>
2020-08-17 14:31:54 -0500

    Add PATH and HOME for aws-cli; Redirect std/err to log file

--- install_s3_uploader.sh
+++ install_s3_uploader.sh
@@ -4,10 +4,12 @@ set -e
 
 _HOME_=${HOME}
 _PWD_=$(pwd)
+_USER_=${USER}
+_PATH_=${PATH}
 
 cat <<EOF >upload_files_to_s3
 # Upload queued files to AWS S3
-*/10 * * * * root . ${_HOME_}/.profile && ${_PWD_}/upload_files_to_s3.sh
+*/10 * * * * ${_USER_} PATH=${_PATH_} . ${_HOME_}/.profile && ${_PWD_}/upload_files_to_s3.sh ${HOME}
 EOF
 
 sudo chown root:root upload_files_to_s3
--- upload_files_to_s3.sh
+++ upload_files_to_s3.sh
@@ -5,6 +5,7 @@
 
 set -e
 
+export HOME=$1
 NOTIFY_DIR=/var/tmp
 LOG_FILE=${NOTIFY_DIR}/s3-updater.log
 QUEUE_DIR=${NOTIFY_DIR}/queue
@@ -25,8 +26,8 @@ OLDEST_FILENAME="$(find $QUEUE_DIR -type f -printf '%T+ %p\n' | sort | head -n 1
 if [[ -n "$OLDEST_FILENAME" ]] ; then
   echo "Found $OLDEST_FILENAME. Waiting 60 seconds in case data being transferred..." >> ${LOG_FILE}
   sleep 60s
-  echo "Uploading $OLDEST_FILENAME to S3..." >> ${LOG_FILE}
-  aws s3 cp --storage-class=STANDARD --force-glacier-transfer --only-show-errors $OLDEST_FILENAME s3://$S3_BUCKET_NAME
+  echo "Uploading $OLDEST_FILENAME to S3 using aws-cli for $HOME..." >> ${LOG_FILE}
+  aws s3 cp --storage-class=STANDARD --force-glacier-transfer $OLDEST_FILENAME s3://$S3_BUCKET_NAME >& ${LOG_FILE}
   echo "Done Uploading. Moving to $COMPLETED_DIR..." >> ${LOG_FILE}
   mv $OLDEST_FILENAME $COMPLETED_DIR
 fi
